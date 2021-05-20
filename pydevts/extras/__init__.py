54429f2325375ffb37874244cfcb69d68465765a
Riley Wilton <meetingprince34@outlook.com>
2021-05-19 21:22:15 -0400

    Working on tests

--- pydevts/extras/__init__.py
+++ pydevts/extras/__init__.py
@@ -5,6 +5,11 @@
 import anyio
 from .. import *
 
+class Cancel(Exception):
+    """
+        Exception
+    """
+    ...
 
 async def server_handler_test(client):
     """
@@ -25,3 +30,23 @@ async def server_handler_test(client):
             
             # Echo data
             await client.send(data)
+
+
+async def tcp_wrapper_test(port):
+    """
+        Test client for test server. Takes a tcp port as an argument
+    """
+    
+    # Create a connection
+    async with await anyio.connect_tcp("localhost", port) as conn:
+        # Wrap the connection
+        connw = Connection(conn)
+        # Send
+        tosend = {"test":"val"}
+        await connw.send(tosend)
+        # Receive
+        recv = await connw.receive()
+        # Assert that we got back the same
+        assert tosend == recv
+    # Basically return
+    raise Cancel()
--- tests.py
+++ tests.py
@@ -16,41 +16,15 @@ import pytest
 
 pytestmark = pytest.mark.anyio
 
-class Cancel(Exception):
-    """
-        Exception
-    """
-    ...
+
 
 async def test_tcp_wrapper():
-    
-    async def tcp_wrapper_test(listener, tg):
-        port = listener.listeners[0]._raw_socket.getsockname()[1]
-        
-        # Create a connection
-        async with await anyio.connect_tcp("localhost", port) as conn:
-
-            # Wrap the connection
-            connw = Connection(conn)
-
-            # Send
-            tosend = {"test":"val"}
-            await connw.send(tosend)
-
-            # Receive
-            recv = await connw.receive()
-
-            # Assert that we got back the same
-            assert tosend == recv
-
-        # Basically return
-        raise Cancel()
-
-        
     try:
         async with anyio.create_task_group() as tg:
             l = await anyio.create_tcp_listener(local_port=0)
+            port = l.listeners[0]._raw_socket.getsockname()[1]
             tg.start_soon(l.serve,server_handler_test)
-            tg.start_soon(tcp_wrapper_test,l, tg)
+            tg.start_soon(tcp_wrapper_test,port)
     except Cancel:
         pass
+
