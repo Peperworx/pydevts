b5347722d2481a5a012ccfcf25067904b5f25056
Riley Wilton <riley.j.wilton@gmail.com>
2021-08-16 15:36:07 -0400

    Looking good.

---
+++ pydevts/conn/message.py
@@ -0,0 +1,8 @@
+"""
+    This file provides a basic implementation of the PYDEVTS Message Protocol
+"""
+
+
+class PYDEVTSMessageProto:
+    def __init__(self):
+        pass
---
+++ testnode.py
@@ -0,0 +1,19 @@
+"""
+    Tests the PYDEVTS Message Protocol
+"""
+from pydevts.conn import PYDEVTSMessageProto
+from anyio import create_task_group, run
+import sys
+
+app = PYDEVTSMessageProto()
+
+async def send_msg():
+    if sys.argv[1] == "send":
+        await app.send_to(sys.argv[2], sys.argv[3])
+
+async def main():
+    async with create_task_group() as tg:
+        tg.start_soon(send_msg)
+        await app.run()
+
+run(main)
